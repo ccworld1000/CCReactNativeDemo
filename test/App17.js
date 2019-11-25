08b11c76ccd1db5982cc90e994ee31183c3ae1df
CC (deng you hua) <ccworld1000@gmail.com>
2019-11-25 14:50:58 +0800

    CCAutoTag : text

---
+++ test/App17.js
@@ -0,0 +1,167 @@
+import React, {Component} from 'react';
+import {ScrollView, Image, Text} from 'react-native';
+
+export default class IScrolledDownAndWhatHappemedNextShockedMe extends Component {
+  render() {
+    return (
+      <ScrollView>
+        <Text style={{fontSize:96}}>Scroll me plz</Text>
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Text style={{fontSize:96}}>If you like</Text>
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Text style={{fontSize:96}}>Scrolling down</Text>
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Text style={{fontSize:96}}>What's the best</Text>
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Text style={{fontSize:96}}>Framework around?</Text>
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Image source={{
+          uri:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574526747056&di=7b31a50adfef793a231d015dcb98431c&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn21%2F440%2Fw640h600%2F20180915%2Fc0f4-hkahyhx4537301.jpg',
+          width: 100,
+          height:100,
+          }}
+        />
+        <Text style={{fontSize:80}}>React Native</Text>
+      </ScrollView>
+      );
+  }
+}
